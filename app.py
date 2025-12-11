ab596683f0a9bc07d98749633dbc164642052a60
yannn-0430 <2564581732@qq.com>
2025-12-11 07:46:43 +0000

    4th end

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-
+from sqlite3.dbapi2 import Timestamp
+import datetime
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
@@ -8,13 +10,36 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    q = request.form.get("q")
-    print(q)
+    name= request.form.get("q")
+    timestamp = datetime.datetime.now()
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("insert into user (name,timestamp) values(?,?)",(name,timestamp))
+    conn.commit()
+    c.close
+    conn.close()
     return(render_template("main.html"))
 
 @app.route("/paynow",methods=["GET","POST"])
 def paynow():
     return(render_template("paynow.html"))
+
+@app.route("/userlog",methods=["GET","POST"])
+def paynow():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("select * from user")
+    r=""
+    for row in c:
+        r = r + str(row)
+    print(r)
+    c.close
+    conn.close()
+    return(render_template("userlog.html"),r=r)
+
+@app.route("/deleteuserlog",methods=["GET","POST"])
+def paynow():
+    return(render_template("deleteuserlog.html"))
 
 if __name__ == "__main__":
     app.run()
